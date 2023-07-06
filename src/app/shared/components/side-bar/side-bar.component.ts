6a12567fe4c2f8c8ca10f9755f64352ed288431e
Dayana Abreu <dayana.abreu@accenture.com>
2023-07-06 16:56:25 -0300

    Side-bar

--- src/app/shared/components/side-bar/side-bar.component.ts
+++ src/app/shared/components/side-bar/side-bar.component.ts
@@ -6,5 +6,64 @@ import { Component } from '@angular/core';
   styleUrls: ['./side-bar.component.css']
 })
 export class SideBarComponent {
+  mainMenu: { //Quiere decir que tiene que contener un objeto por defecto
+    defaultOptions: Array<any>, accessLink: Array<any> }//va a tener dos propiedades, defaultoptions y accesslink
+    //A su vez, cada propiedad debe contener una lista de arrays
+    //Así solo va a dar error en main Menu, hay que inicializarlo poniendo:
+    = {defaultOptions: [], accessLink: []}
 
+    customOptions: Array<any> = []
+
+    constructor() { }
+
+  ngOnInit(): void {
+    this.mainMenu.defaultOptions = [
+      {
+        name: 'Home',
+        icon: 'uil uil-estate',
+        router: ['/']
+      },
+      {
+        name: 'Buscar',
+        icon: 'uil uil-search',
+        router: ['/', 'history']
+      },
+      {
+        name: 'Tu biblioteca',
+        icon: 'uil uil-chart',
+        router: ['/', 'favorites'],
+      }
+    ]
+    this.mainMenu.accessLink = [
+      {
+        name: 'Crear lista',
+        icon: 'uil-plus-square'
+      },
+      {
+        name: 'Canciones que te gustan',
+        icon: 'uil-heart-medical'
+      }
+    ]
+
+    this.customOptions = [
+      {
+        name: 'Mi lista º1',
+        router: ['/']
+      },
+      {
+        name: 'Mi lista º2',
+        router: ['/']
+      },
+      {
+        name: 'Mi lista º3',
+        router: ['/']
+      },
+      {
+        name: 'Mi lista º4',
+        router: ['/']
+      }
+    ]
+
+  }
 }
+
