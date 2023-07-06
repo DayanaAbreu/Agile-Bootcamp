d00b9155898e348d7e264710d844665fbf23244f
Dayana Abreu <dayana.abreu@accenture.com>
2023-07-06 13:53:54 -0300

    Update routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: '', //TODO: localhost:4200/
+    loadChildren: () =>import(`./modules/home/home.module`).then(m=>m.HomeModule)
   } 
 ];
 
