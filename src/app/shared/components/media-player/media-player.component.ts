1b106cc5f3bc5b3d0ef1ed47570beabf96a4ada8
Dayana Abreu <dayana.abreu@accenture.com>
2023-07-07 13:10:06 -0300

    ng templates-container

--- src/app/shared/components/media-player/media-player.component.ts
+++ src/app/shared/components/media-player/media-player.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./media-player.component.css']
 })
 export class MediaPlayerComponent {
-
+  mockCover: any = {
+    cover: 'https://i.scdn.co/image/ab67616d0000b27345ca41b0d2352242c7c9d4bc',
+    album: 'BEBE (Oficial',
+    name: 'Gioli & Assia'
+  }
 }
