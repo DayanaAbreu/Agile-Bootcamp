616a9e0230eb3f694c7123e3f700197254938dfd
Dayana Abreu <dayana.abreu@accenture.com>
2023-07-07 16:06:08 -0300

    CSS updated

--- src/app/shared/components/card-player/card-player.component.ts
+++ src/app/shared/components/card-player/card-player.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from '@angular/core';
+import { TrackModel } from '@core/models/tracks.model';
 
 @Component({
   selector: 'app-card-player',
@@ -6,6 +7,6 @@ import { Component, Input } from '@angular/core';
   styleUrls: ['./card-player.component.css']
 })
 export class CardPlayerComponent {
-  @Input() mode:string=''
-  @Input() track:string=''
+  @Input() mode:'small' | 'big' = 'small'
+  @Input() track: TrackModel = { _id: 0, name: '', album: '', url: '', cover: ''};
 }
