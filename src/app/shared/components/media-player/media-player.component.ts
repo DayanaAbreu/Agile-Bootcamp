7537dccb06965d5159c4371276bd72e6e6e5d78b
Dayana Abreu <dayana.abreu@accenture.com>
2023-07-17 18:11:40 -0300

    Reproductor

--- src/app/shared/components/media-player/media-player.component.ts
+++ src/app/shared/components/media-player/media-player.component.ts
@@ -1,37 +1,42 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { TrackModel } from '@core/models/tracks.model';
 import { MultimediaService } from '@shared/service/multimedia.service';
-import { Subscription } from 'rxjs';
+import { Subscription } from 'rxjs'; //TODO: Programacion reactiva!
 
 @Component({
   selector: 'app-media-player',
   templateUrl: './media-player.component.html',
   styleUrls: ['./media-player.component.css']
 })
-export class MediaPlayerComponent implements OnInit, OnDestroy{
-  mockCover: TrackModel = {
-    cover: 'https://i.scdn.co/image/ab67616d0000b27345ca41b0d2352242c7c9d4bc',
-    album: 'BEBE (Oficial',
-    name: 'Gioli & Assia',
-    url: 'http://localhost/track.mp3',
-    _id: 1
-  }
-
+export class MediaPlayerComponent implements OnInit, OnDestroy {
+  @ViewChild('progressBar') progressBar: ElementRef = new ElementRef('')
   listObservers$: Array<Subscription> = []
+  state: string = 'paused'
+  constructor(public multimediaService: MultimediaService) { }
 
-  constructor(private multimediaService: MultimediaService) {}
-  
   ngOnInit(): void {
-    const observer1$: Subscription = this.multimediaService.callback.subscribe(
-      (response: TrackModel) => {
-        console.log('Recibiendo cancion...', response);
-      }
-    )
+
+    const observer1$ = this.multimediaService.playerStatus$
+      .subscribe(status => this.state = status)
     this.listObservers$ = [observer1$]
   }
 
   ngOnDestroy(): void {
     this.listObservers$.forEach(u => u.unsubscribe())
-    console.log('_______BOOOM!');
+    console.log('🔴🔴🔴🔴🔴🔴🔴 BOOM!');
   }
+
+
+  handlePosition(event: MouseEvent): void {
+    const elNative: HTMLElement = this.progressBar.nativeElement
+    const { clientX } = event
+    const { x, width } = elNative.getBoundingClientRect()
+    const clickX = clientX - x //TODO: 1050 - x
+    const percentageFromX = (clickX * 100) / width
+    console.log(`Click(x): ${percentageFromX}`);
+    this.multimediaService.seekAudio(percentageFromX)
+
+  }
+
+
 }
