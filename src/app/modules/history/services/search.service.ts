4c3f7afc90160ea39f778a7b40082726545886b4
DayanaAbreu <dayana.abreu@accenture.com>
2023-07-24 13:54:56 -0300

    history check

--- src/app/modules/history/services/search.service.ts
+++ src/app/modules/history/services/search.service.ts
@@ -3,7 +3,6 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
-import { TrackModel } from '@core/models/tracks.model';
 
 @Injectable({
   providedIn: 'root'
@@ -13,7 +12,7 @@ export class SearchService {
 
   constructor(private http: HttpClient) { }
 
-  searchTracks$(term: string): Observable<TrackModel> {
+  searchTracks$(term: string): Observable<any> {
     return this.http.get(`${this.URL}/tracks?src=${term}`)
       .pipe(
         map((dataRaw: any) => dataRaw.data)
